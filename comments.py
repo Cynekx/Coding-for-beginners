f56e41d8041c5abe94e7b56e11c1510b1eca2f59
Cynekx <105606992+Cynekx@users.noreply.github.com>
2022-05-15 21:39:01 +0200

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,26 @@
+#Code for section comments
+''' This is
+making few lines
+of a comment
+'''
+
+userName = input("What's your name?") # this is only for code typed in one line (commenting)
+print("Hello "+ userName)
+
+response = input("Do you want to know my name?") # action required
+
+if response == "yes":
+      print("It's Reina, nice to meet you :)")
+else:
+    print("okay...")
+    print("Let's move on then")
+
+
+
+print("I have a qestion for you")
+response = input("Do you know what boolean is?")
+
+if response == "yes":
+    print("great!")
+else:
+    print("it says if the value is ture or false")
---
+++ howcoderuns.py
@@ -0,0 +1,8 @@
+import time
+print("Get readyyy")
+time.sleep(3)
+print("3..")
+time.sleep(2)
+print("2....")
+time.sleep(1)
+print("1..  go!")
