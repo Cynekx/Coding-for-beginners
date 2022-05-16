b8d657ae80b3bd2507aee97079ae568cd96eec8c
Cynekx <105606992+Cynekx@users.noreply.github.com>
2022-05-16 19:50:06 +0200

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,54 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(1+1)
+2
+print(1+2*3)
+7
+print(120-545)
+-425
+print(1>2)
+False
+print(32>12)
+True
+print(100^2)
+102
+print(100>=2)
+True
+print(1=23
+      
+SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+print(1=23)
+      
+SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+print(1==23)
+      
+False
+print("= oznacza przypisanie danej, == porownuje dane")
+      
+= oznacza przypisanie danej, == porownuje dane
+liczba = 100
+      
+print(liczba)
+      
+100
+liczba == 1234
+      
+False
+liczba == 1--
+      
+SyntaxError: invalid syntax
+liczba == 100
+      
+True
+100 and 1 > 100
+      
+False
+100 and 50 < 70
+      
+True
+100 and 50 >60
+      
+False
+print("Hello" + "Cynia")
+      
+HelloCynia
