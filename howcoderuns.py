981b460ae8807e3eb6b7d4a137ff7c1e2b5c30c1
Cynekx <105606992+Cynekx@users.noreply.github.com>
2022-06-25 20:47:44 +0200

    Update howcoderuns.py

--- howcoderuns.py
+++ howcoderuns.py
@@ -6,3 +6,4 @@ time.sleep(2)
 print("2....")
 time.sleep(1)
 print("1..  go!")
+''' inspired by Udemy course " The perfect course for complete beginners. Friendly - No experience required. Go from scratch to coding a real app!" '''
